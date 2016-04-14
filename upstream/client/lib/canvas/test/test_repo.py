02ca3cd6093a50934c0b1998d3b553c145e31056
Lithrem <lithrem@kororaproject.org>
2016-04-14 10:08:21 +1000

    added: Inital parse_str tests for repository

--- upstream/client/lib/canvas/test/test_repo.py
+++ upstream/client/lib/canvas/test/test_repo.py
@@ -13,29 +13,133 @@ class RepoTestCase(TestCase):
     def setUp(self):
         pass
 
-    def test_repo_empty(self):
+    def test_repo_parse_empty(self):
         r1 = Repository({})
 
         self.assertEqual(None, r1.name)
         self.assertEqual(None, r1.stub)
+
         self.assertEqual(None, r1.baseurl)
         self.assertEqual(None, r1.mirrorlist)
         self.assertEqual(None, r1.metalink)
-        self.assertEqual(None, r1.enabled)
+
         self.assertEqual(None, r1.gpgkey)
+        self.assertEqual(None, r1.enabled)
         self.assertEqual(None, r1.gpgcheck)
         self.assertEqual(None, r1.cost)
+        self.assertEqual(False, r1.install)
+
+        self.assertEqual(None, r1.exclude_packages)
+        self.assertEqual(None, r1.include_packages)
+
         self.assertEqual(None, r1.priority)
+
         self.assertEqual(None, r1.meta_expired)
-        self.assertEqual(Repository.ACTION_INCLUDE, r1.action)
 
-    def test_repo_action(self):
-        r1 = Repository('foo')
-        r2 = Repository('~foo')
+        self.assertEqual(r1.action, Repository.ACTION_INCLUDE)
 
-        self.assertEqual(Repository.ACTION_INCLUDE, r1.action)
-        self.assertEqual(Repository.ACTION_EXCLUDE, r2.action)
+    def test_repo_kwargs(self):
+        pass
 
+# https://github.com/rhinstaller/pykickstart/blob/master/docs/kickstart-docs.rst#repo
+    def test_repo__parse_str_arg(self):
+        # Double Quotes
+        self.assertEqual(Repository._parse_str_arg('name="korora"', 'name='), 'korora')
+        # No Quotes
+        self.assertEqual(Repository._parse_str_arg('name=korora 23', 'name='), 'korora 23')
+        # Single Quotes
+        self.assertEqual(Repository._parse_str_arg("name='korora 23'", 'name='), 'korora 23')
+
+    def test_repo_parse_str_name(self):
+        r1 = Repository('repo --name="Korora 23 - i386 - Updates"')
+        r2 = Repository('repo --baseurl=http://download1.rpmfusion.org/ --cost=1000')
+
+        # Name set
+        self.assertEqual('Korora 23 - i386 - Updates', r1.name)
+
+        # Name unset
+        self.assertEqual(None, r2.name)
+
+    def test_repo_parse_str_baseurl(self):
+        r1 = Repository('repo --baseurl="https://fakeurl"')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Set
+        self.assertEqual('https://fakeurl', r1.baseurl)
+
+        # Unset
+        self.assertEqual(None, r2.baseurl)
+
+    def test_repo_parse_str_mirrorlist(self):
+        r1 = Repository('repo --mirrorlist="https://fakeurl"')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Set
+        self.assertEqual('https://fakeurl', r1.mirrorlist)
+
+        # Unset
+        self.assertEqual(None, r2.mirrorlist)
+
+    def test_repo_parse_str_proxy(self):
+        r1 = Repository('repo --proxy="https://fakeproxy"')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Set
+        self.assertEqual('https://fakeproxy', r1.proxy)
+
+        # Unset
+        self.assertEqual(None, r2.proxy)
+
+    def test_repo_parse_str_ignoregroups(self):
+        r1 = Repository('repo --name="Korora 23 - i386 - Updates" --ignoregroups=true')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates" --ignoregroups')
+        r3 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Set
+        self.assertEqual(True, r1.ignoregroups)
+
+        # fedora documentation implies that it must be --ignoregroups=true 
+        self.assertEqual(False, r2.ignoregroups)
+
+        # Unset
+        self.assertEqual(False, r3.ignoregroups)
+
+
+    def test_repo_parse_str_noverifyssl(self):
+        r1 = Repository('repo --name="Korora 23 - i386 - Updates" --noverifyssl')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Set
+        self.assertEqual(True, r1.noverifyssl)
+
+        # Unset
+        self.assertEqual(False, r2.noverifyssl)
+
+    def test_repo_parse_str_install(self):
+        r1 = Repository('repo --name="Korora 23 - i386 - Updates" --install')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Set
+        self.assertEqual(True, r1.install)
+
+        # Unset
+        self.assertEqual(False, r2.install)
+
+
+    def test_repo_parse_str_cost(self):
+        r1 = Repository('repo --cost="1"')
+        r2 = Repository('repo --name="Korora 23 - i386 - Updates"')
+
+        # Double Quoted
+        self.assertEqual('1', r1.cost)
+
+        # Unset
+        self.assertEqual(None, r2.cost)
+
+    def test_repo_parse_str(self):
+        # TODO: Full data population here in various orders r1 == r2
+        # Attempting to see if name="Fedora--foo" breaks things
+        pass
 
     def test_repo_equality(self):
         r1 = Repository({'s': 'foo'})
